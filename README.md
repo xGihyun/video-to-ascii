6e1bbc4505d34d637c97c26767ad3295df4d59c2
xGihyun <giordnnuz@gmail.com>
2025-07-05 08:40:46 +0800

    play audio and use buffered writer for output

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Video to ASCII
 
-WIP: Fun little project with Zig.
+A fun little project with Zig. 
+
+I initially created this project for fun to play [Bad Apple](https://www.youtube.com/watch?v=FtutLA63Cp8) on the terminal. 
+This type of project has already been done by a lot of other people, but it feels different when you play it with the code you wrote yourself.
+
+> [!WARNING]
+> This project is unpolished (i.e., hard coded values).
 
 ### Prerequisites
 
-- [ffmpeg](https://www.ffmpeg.org/) for extracking video frames.
-- [magick](https://imagemagick.org/) for converting a single image in any format to `.bmp`.
+- [ffmpeg](https://www.ffmpeg.org/) for extracting video frames and audio.
+- [vlc](https://www.videolan.org/vlc/) for playing the audio.
--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,9 @@ pub fn main() !void {
         error.PathAlreadyExists => {},
         else => return err,
     };
-    defer std.fs.cwd().deleteTree(temp_dir) catch {};
+    defer std.fs.cwd().deleteTree(temp_dir) catch {
+        std.debug.print("Failed to delete temporary frames directory.\n", .{});
+    };
 
     std.debug.print("Extracting frames from video...\n", .{});
     try extractFrames(allocator, "/home/gihyun/Development/video-to-ascii/videos/bad-apple.mp4", temp_dir);
@@ -29,36 +31,68 @@ pub fn main() !void {
 
     std.debug.print("Getting frame files...\n", .{});
     try getFrameFiles(allocator, temp_dir, &frame_files);
+    std.debug.print("Found {d} frames.\n", .{frame_files.items.len});
 
-    std.debug.print("Found {d} frames. Starting playback...\n", .{frame_files.items.len});
-    for (frame_files.items) |frame| {
-        try processFrame(allocator, frame);
+    std.debug.print("Preloading ASCII frames...\n", .{});
+    const frames = try preloadAsciiFrames(allocator, frame_files.items);
+    std.debug.print("Success.\n", .{});
+
+    std.debug.print("Extracting audio...\n", .{});
+    try extractAudio(allocator);
+    std.debug.print("Success.\n", .{});
+
+    // ???
+    std.debug.print("\x1b[2J\x1b[?25l\x1b[?1049h", .{});
+    defer {
+        std.debug.print("\x1b[?1049l\x1b[?25h", .{});
+        std.debug.print("Playback finished.\n", .{});
+    }
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    std.debug.print("Starting playback...\n", .{});
+    std.time.sleep(1 * std.time.ns_per_s);
+
+    var audio_process = try playAudio(allocator);
+    defer {
+        _ = audio_process.kill() catch {
+            std.debug.print("Failed to kill audio process.\n", .{});
+        };
+        std.fs.cwd().deleteFile("output.m4a") catch {};
+    }
+
+    for (frames) |frame| {
+        try stdout.print("\x1b[H{s}", .{frame});
+        try bw.flush();
         std.time.sleep(FRAME_DURATION_MS * std.time.ns_per_ms);
     }
 }
 
-fn asciiToImage(allocator: std.mem.Allocator) !void {
-    const convert_cmd = [_][]const u8{ "magick", "/home/gihyun/Development/video-to-ascii/images/mika-xD.jpg", "bmp:-" };
+// A function iniitally used for image to ASCII
+// fn asciiToImage(allocator: std.mem.Allocator) !void {
+//     const convert_cmd = [_][]const u8{ "magick", "/home/gihyun/Development/video-to-ascii/images/mika-xD.jpg", "bmp:-" };
+//
+//     var child = std.process.Child.init(&convert_cmd, allocator);
+//     child.stdout_behavior = .Pipe;
+//     child.stderr_behavior = .Pipe;
+//
+//     try child.spawn();
+//
+//     const stdout = try child.stdout.?.readToEndAlloc(allocator, std.math.maxInt(usize));
+//     defer allocator.free(stdout);
+//
+//     const term = try child.wait();
+//     if (term.Exited != 0) {
+//         std.debug.print("Error\n", .{});
+//         return;
+//     }
+//
+//     try bmpToAscii(stdout, ASCII_WIDTH, ASCII_HEIGHT);
+// }
 
-    var child = std.process.Child.init(&convert_cmd, allocator);
-    child.stdout_behavior = .Pipe;
-    child.stderr_behavior = .Pipe;
-
-    try child.spawn();
-
-    const stdout = try child.stdout.?.readToEndAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(stdout);
-
-    const term = try child.wait();
-    if (term.Exited != 0) {
-        std.debug.print("Error\n", .{});
-        return;
-    }
-
-    try bmpToAscii(stdout, ASCII_WIDTH, ASCII_HEIGHT);
-}
-
-fn bmpToAscii(data: []const u8, ascii_width: u32, ascii_height: u32) !void {
+fn bmpToAscii(allocator: std.mem.Allocator, data: []const u8, ascii_width: u32, ascii_height: u32) ![]const u8 {
     const width = std.mem.readInt(u32, data[18..22], .little);
     const height = std.mem.readInt(u32, data[22..26], .little);
     const data_offset = std.mem.readInt(u32, data[10..14], .little);
@@ -66,6 +100,9 @@ fn bmpToAscii(data: []const u8, ascii_width: u32, ascii_height: u32) !void {
     const pixel_data = data[data_offset..];
     const x_ratio = @as(f32, @floatFromInt(width)) / @as(f32, @floatFromInt(ascii_width));
     const y_ratio = @as(f32, @floatFromInt(height)) / @as(f32, @floatFromInt(ascii_height));
+
+    var chars = std.ArrayList(u8).init(allocator);
+    defer chars.deinit();
 
     var y: u32 = 0;
     while (y < ascii_height) : (y += 1) {
@@ -84,14 +121,17 @@ fn bmpToAscii(data: []const u8, ascii_width: u32, ascii_height: u32) !void {
                 const r = pixel_data[pixel_idx + 2];
 
                 const ascii_char = pixelToAscii(r, g, b);
-                std.debug.print("{c}", .{ascii_char});
+                try chars.append(ascii_char);
             } else {
-                std.debug.print(" ", .{});
+                try chars.append(' ');
             }
         }
 
-        std.debug.print("\n", .{});
+        try chars.append('\n');
     }
+
+    const str = try chars.toOwnedSlice();
+    return str;
 }
 
 fn pixelToAscii(r: u8, g: u8, b: u8) u8 {
@@ -136,15 +176,49 @@ fn compareStrings(context: void, a: []const u8, b: []const u8) bool {
     return std.mem.lessThan(u8, a, b);
 }
 
-fn processFrame(allocator: std.mem.Allocator, frame_file: []const u8) !void {
-    const file = try std.fs.cwd().openFile(frame_file, .{});
-    defer file.close();
+fn extractAudio(allocator: std.mem.Allocator) !void {
+    const cmd = [_][]const u8{ "ffmpeg", "-i", "/home/gihyun/Development/video-to-ascii/videos/bad-apple.mp4", "-vn", "-acodec", "copy", "-y", "output.m4a" };
 
-    const file_size = try file.getEndPos();
-    const data = try allocator.alloc(u8, file_size);
-    defer allocator.free(data);
+    var child = std.process.Child.init(&cmd, allocator);
+    child.stdout_behavior = .Ignore;
+    child.stderr_behavior = .Ignore;
 
-    _ = try file.readAll(data);
+    const term = try child.spawnAndWait();
+    if (term.Exited != 0) {
+        std.debug.print("Failed to extract audio.\n", .{});
+        return;
+    }
+}
 
-    try bmpToAscii(data, ASCII_WIDTH, ASCII_HEIGHT);
+fn playAudio(allocator: std.mem.Allocator) !std.process.Child {
+    const cmd = [_][]const u8{ "cvlc", "output.m4a" };
+
+    var child = std.process.Child.init(&cmd, allocator);
+    child.stdout_behavior = .Ignore;
+    child.stderr_behavior = .Ignore;
+
+    try child.spawn();
+
+    return child;
+}
+
+fn preloadAsciiFrames(allocator: std.mem.Allocator, frame_files: [][]const u8) ![][]const u8 {
+    var frames = try std.ArrayList([]const u8).initCapacity(allocator, frame_files.len);
+    defer frames.deinit();
+
+    for (frame_files) |path| {
+        const file = try std.fs.cwd().openFile(path, .{});
+        defer file.close();
+
+        const file_size = try file.getEndPos();
+        const data = try allocator.alloc(u8, file_size);
+        defer allocator.free(data);
+
+        _ = try file.readAll(data);
+
+        const ascii_frame = try bmpToAscii(allocator, data, ASCII_WIDTH, ASCII_HEIGHT);
+        try frames.append(ascii_frame);
+    }
+
+    return frames.toOwnedSlice();
 }
